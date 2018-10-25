8a9c1400b839ba9b3bee9d82ac73455b438a4710
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2018-10-25 20:35:49 +0300

    16-add-echo-persistance

--- cm/cm-app/src/main/java/com/dj/cm/Application.java
+++ cm/cm-app/src/main/java/com/dj/cm/Application.java
@@ -1,8 +1,9 @@
-package com.dj.cm.app;
+package com.dj.cm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@EnableJpaRepositories("com.dj.cm.persistence.repo")
 @SpringBootApplication(scanBasePackages = "com.dj.cm")
 public class Application {
 	public static void main(String[] args) {
