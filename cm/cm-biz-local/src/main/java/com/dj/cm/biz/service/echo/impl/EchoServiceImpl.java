fc50f6ee555c2dadc6e153b406c3bd7b67ec27c4
vkazhdan <vkazhdan@anotheria.net>
2018-10-29 12:42:41 +0200

    16-add-echo-persistance: Echo service extending with create/update/delete

--- cm/cm-biz-local/src/main/java/com/dj/cm/biz/service/echo/impl/EchoServiceImpl.java
+++ cm/cm-biz-local/src/main/java/com/dj/cm/biz/service/echo/impl/EchoServiceImpl.java
@@ -1,11 +1,15 @@
 package com.dj.cm.biz.service.echo.impl;
 
 import com.dj.cm.biz.service.echo.EchoService;
+import com.dj.cm.biz.service.exception.NotFoundBizException;
 import com.dj.cm.model.entity.Echo;
 import com.dj.cm.persistence.repo.echo.EchoRepository;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EchoServiceImpl implements EchoService {
@@ -20,7 +24,30 @@ public class EchoServiceImpl implements EchoService {
 
 	@Override
 	public Echo getEchoById(Long id) {
-		return echoRepository.findById(id).orElse(null);
+		Optional<Echo> echoOptional = echoRepository.findById(id);
+		if (!echoOptional.isPresent()) {
+			throw new NotFoundBizException(String.format("Echo with Id: %s not found", id));
+		}
+
+		return echoOptional.get();
+	}
+
+	@Override
+	public Echo createEcho(Echo echo) {
+		return echoRepository.save(echo);
+	}
+
+	@Override
+	public Echo updateEcho(Echo echo) {
+		Echo echoToUpdate = getEchoById(echo.getId());
+		BeanUtils.copyProperties(echo, echoToUpdate, "id");
+		return echoRepository.save(echoToUpdate);
+	}
+
+	@Override
+	public void deleteEcho(Long id) {
+		Echo echoToDelete = getEchoById(id);
+		echoRepository.delete(echoToDelete);
 	}
 
 	@Override
@@ -31,10 +58,6 @@ public class EchoServiceImpl implements EchoService {
 	@Override
 	public String doEcho(Long id, int n) {
 		Echo echo = getEchoById(id);
-		if (echo != null) {
-			return doEcho(echo.getValue(), n);
-		} else {
-			return "Not found";
-		}
+		return doEcho(echo.getValue(), n);
 	}
 }
