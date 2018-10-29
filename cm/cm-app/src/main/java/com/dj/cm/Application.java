248c85c9862a61497183b5ab649064450b033583
vkazhdan <vkazhdan@anotheria.net>
2018-10-29 08:40:02 +0200

    16-add-echo-persistance: Moved persistence config to separate properties

--- cm/cm-app/src/main/java/com/dj/cm/Application.java
+++ cm/cm-app/src/main/java/com/dj/cm/Application.java
@@ -3,7 +3,6 @@ package com.dj.cm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@EnableJpaRepositories("com.dj.cm.persistence.repo")
 @SpringBootApplication(scanBasePackages = "com.dj.cm")
 public class Application {
 	public static void main(String[] args) {
