d319f5bc31034d9308d891280a762b0d63698199
vkazhdan <vkazhdan@anotheria.net>
2018-10-29 09:12:24 +0200

    16-add-echo-persistance: Extended echo service and rest

--- cm/cm-biz/src/main/java/com/dj/cm/biz/service/echo/EchoService.java
+++ cm/cm-biz/src/main/java/com/dj/cm/biz/service/echo/EchoService.java
@@ -1,19 +1,44 @@
 package com.dj.cm.biz.service.echo;
 
+import com.dj.cm.model.entity.Echo;
+
+import java.util.List;
+
 /**
  * Echo Service.
  */
 public interface EchoService {
 
-    /**
-     * Do echo of string.
-     *
-     * @param s value for string
-     * @param n times to echo
-     * @return echo string
-     */
-    String doEcho(String s, int n);
+	/**
+	 * Do echo of string.
+	 *
+	 * @param s value for string
+	 * @param n times to echo
+	 * @return echo string
+	 */
+	String doEcho(String s, int n);
 
-    String getEcho(Long id, int n);
+	/**
+	 * Do echo by id.
+	 *
+	 * @param id echo id
+	 * @param n  times to echo
+	 * @return echo string
+	 */
+	String doEcho(Long id, int n);
 
+	/**
+	 * Get all echos;
+	 *
+	 * @return @{@link Echo} list
+	 */
+	Iterable<Echo> getAllEchos();
+
+	/**
+	 * Get echo by id;
+	 *
+	 * @param id get by id
+	 * @return echo or null if not found
+	 */
+	Echo getEchoById(Long id);
 }
