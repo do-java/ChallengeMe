472ca955122f952bf3c29a354b832f5e9fa6230f
Andrey Khudyashov <kompromis87@gmail.com>
2018-11-01 12:53:27 +0200

    18-add-user-service Add exception to user repo

--- cm/cm-persistence/src/main/resources/db/migration/V001__Add_echo_table.sql
+++ cm/cm-persistence/src/main/resources/db/migration/V001__Add_echo_table.sql
@@ -13,3 +13,11 @@ CREATE TABLE user_model(
  first_login DATE NOT NULL DEFAULT CURRENT_DATE,
  last_login DATE NOT NULL DEFAULT CURRENT_DATE
 );
+
+INSERT INTO user_model VALUES
+    (1, 'Bananas', '105', '1971-07-13');
+INSERT INTO user_model VALUES (2, 'Bananas', '106', '1971-07-13');
+INSERT INTO user_model VALUES
+    (3, 'Bananas', '107', '1971-07-13');
+INSERT INTO user_model VALUES
+    (4, 'Bananas', '108', '1971-07-13');
