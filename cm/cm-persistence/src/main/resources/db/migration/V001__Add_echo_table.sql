de130f6222c833e79ea5f71d182079e038b6e21c
vkazhdan <vkazhdan@anotheria.net>
2018-11-01 19:23:47 +0200

    18-add-user-service: Updated migration script and rest path
    (cherry picked from commit 45adeed)

--- cm/cm-persistence/src/main/resources/db/migration/V001__Add_echo_table.sql
+++ cm/cm-persistence/src/main/resources/db/migration/V001__Add_echo_table.sql
@@ -2,22 +2,3 @@ CREATE TABLE echo (
  id serial PRIMARY KEY,
  value VARCHAR (250) UNIQUE NOT NULL
 );
-
-CREATE TABLE user_model(
- id serial PRIMARY KEY,
- user_name VARCHAR (250) NOT NULL,
- email VARCHAR (250) UNIQUE NOT NULL,
- password VARCHAR (250) NOT NULL,
- confirmed boolean NOT NULL DEFAULT false,
- deleted boolean NOT  NULL DEFAULT false,
- first_login DATE NOT NULL DEFAULT CURRENT_DATE,
- last_login DATE NOT NULL DEFAULT CURRENT_DATE
-);
-
-INSERT INTO user_model VALUES
-    (1, 'Bananas', '105', '1971-07-13');
-INSERT INTO user_model VALUES (2, 'Bananas', '106', '1971-07-13');
-INSERT INTO user_model VALUES
-    (3, 'Bananas', '107', '1971-07-13');
-INSERT INTO user_model VALUES
-    (4, 'Bananas', '108', '1971-07-13');
