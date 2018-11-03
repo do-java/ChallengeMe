9685151228a4c4103c7d06bf39c9d9af244e0b1c
Andrey Khudyashov <kompromis87@gmail.com>
2018-11-03 21:37:36 +0200

    18-add-user-service Fix UserService. Add Exceptions

--- cm/cm-persistence/src/main/java/com/dj/cm/persistence/repo/user/UserRepository.java
+++ cm/cm-persistence/src/main/java/com/dj/cm/persistence/repo/user/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<UserModel, Long> {
+    UserModel findUserModelByEmail (String userEmail);
 }
