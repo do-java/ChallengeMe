cdd78e68dd24fdce5891226fb965492390cfbf5d
vkazhdan <vkazhdan@anotheria.net>
2018-11-10 12:25:27 +0200

    vkazhdan 23-add-echo-service-cache: Added annotation based cache
    (cherry picked from commit 524ed9c60dff22608d3c332c23535927b60f6342)

--- cm/cm-biz-local/src/main/java/com/dj/cm/biz/service/echo/impl/EchoServiceCachedImpl.java
+++ cm/cm-biz-local/src/main/java/com/dj/cm/biz/service/echo/impl/EchoServiceCachedImpl.java
@@ -7,17 +7,30 @@ import com.dj.cm.persistence.repo.echo.EchoRepository;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-@Primary
-public class EchoServiceImpl implements EchoService {
+@Qualifier("cached")
+@CacheConfig(cacheNames = "com.dj.cm.biz.service.echo")
+public class EchoServiceCachedImpl implements EchoService {
 
 	@Autowired
 	private EchoRepository echoRepository;
+
+	@Autowired
+	private CacheManager cacheManager;	// Can be used for Debug or manual cache control
+
+	@Autowired
+	@Qualifier("cached")
+	private EchoService self; // Used to call self Cacheable methods
 
 	@Override
 	public Iterable<Echo> getAllEchos() {
@@ -25,6 +38,7 @@ public class EchoServiceImpl implements EchoService {
 	}
 
 	@Override
+	@Cacheable
 	public Echo getEchoById(Long id) {
 		Optional<Echo> echoOptional = echoRepository.findById(id);
 		if (!echoOptional.isPresent()) {
@@ -40,15 +54,18 @@ public class EchoServiceImpl implements EchoService {
 	}
 
 	@Override
+	@CachePut(key = "#echo.id")
 	public Echo updateEcho(Echo echo) {
-		Echo echoToUpdate = getEchoById(echo.getId());
-		BeanUtils.copyProperties(echo, echoToUpdate, "id", "createdDate", "modifiedDate");
-		return echoRepository.save(echoToUpdate);
+		Echo echoToUpdate = self.getEchoById(echo.getId());
+		BeanUtils.copyProperties(echo, echoToUpdate, "id");
+		Echo savedEcho = echoRepository.save(echoToUpdate);
+		return savedEcho;
 	}
 
 	@Override
+	@CacheEvict
 	public void deleteEcho(Long id) {
-		Echo echoToDelete = getEchoById(id);
+		Echo echoToDelete = self.getEchoById(id);
 		echoRepository.delete(echoToDelete);
 	}
 
@@ -59,7 +76,7 @@ public class EchoServiceImpl implements EchoService {
 
 	@Override
 	public String doEcho(Long id, int n) {
-		Echo echo = getEchoById(id);
+		Echo echo = self.getEchoById(id);
 		return doEcho(echo.getValue(), n);
 	}
 }
