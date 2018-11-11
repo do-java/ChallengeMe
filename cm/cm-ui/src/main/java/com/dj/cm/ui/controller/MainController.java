3849a245b2813a5570bac0b01505644be3b1c9b7
vkazhdan <vkazhdan@anotheria.net>
2018-11-11 11:38:06 +0200

    27-add-challenge-page: Install npm css-loader

---
+++ cm/cm-ui/src/main/java/com/dj/cm/ui/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.dj.cm.ui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+	@GetMapping
+	public String main(Model model) {
+		return "index";
+	}
+}
