4f2e6bce0c5ecbffe08a3db724744cb01b8310af
vkazhdan <vkazhdan@anotheria.net>
2018-11-18 21:18:06 +0200

    27-add-challenge-page: Set initial value for startDate

--- cm/cm-ui/src/main/resources/static/js/pages/ChallengeAddPage.vue
+++ cm/cm-ui/src/main/resources/static/js/pages/ChallengeAddPage.vue
@@ -2,7 +2,7 @@
 	<div class="card">
 	   <h2 class="card-header">Challenge Add</h2>
 	   <div class="card-body">
-		   <challenge-edit-form :item="{id: null, name:'Sport Event', type:'Sport', description:'UEFA championship', priority:4, maxMembers:8, access:'PRIVATE', status:'ACTIVE', startDate: '2019-01-18 18:30', endDate: '2019-01-25 21:30'}"></challenge-edit-form>
+		   <challenge-edit-form :item="{id: null, name:'Sport Event', type:'Sport', description:'UEFA championship', priority:4, maxMembers:8, access:'PRIVATE', status:'ACTIVE', startDate: '2018-11-15T16:30:00.000+0000', endDate: null}"></challenge-edit-form>
 		</div>
    </div>
 </template>
