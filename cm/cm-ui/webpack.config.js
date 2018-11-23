719a3cdb237211c98a11174e8c972cd396ef7795
vkazhdan <vkazhdan@anotheria.net>
2018-11-23 13:22:22 +0200

    31-ui-design-common: Added scss support

--- cm/cm-ui/webpack.config.js
+++ cm/cm-ui/webpack.config.js
@@ -29,13 +29,21 @@ module.exports = {
         test: /\.vue$/,
         loader: 'vue-loader'
       },
-		{
-		   test: /\.css$/,
-		   use: [
-			   'vue-style-loader',
-			   'css-loader'
-		   ]
-		}
+	{
+	   test: /\.css$/,
+	   use: [
+		   'vue-style-loader',
+		   'css-loader'
+	   ]
+	},
+	{
+		test: /\.scss$/,
+		use: [
+		  'vue-style-loader',
+		  'css-loader',
+		  'sass-loader'
+		]
+	  }
     ]
   },
   plugins: [
