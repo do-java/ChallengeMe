875170e716c4142a7fadd7daf27a38d24986b371
vkazhdan <vkazhdan@anotheria.net>
2018-11-26 21:39:34 +0200

    31-ui-design-common: Navigation highlight

--- cm/cm-ui/src/main/resources/static/js/components/common/Navigation.vue
+++ cm/cm-ui/src/main/resources/static/js/components/common/Navigation.vue
@@ -2,8 +2,8 @@
 	<nav class="navbar navbar-expand-sm navbar-light bg-primary mb-4 shadow">
 		<router-link class="navbar-brand cm-logo" to="/">Challenge<span class="cm-logo-me">Me</span></router-link>
 		<ul class="navbar-nav">
-			<router-link class="navbar-item nav-link" active-class="active font-weight-bold" to="/echo">Echo</router-link>
-			<router-link class="navbar-item nav-link" active-class="active font-weight-bold" to="/challenge">Challenge</router-link>
+			<router-link class="navbar-item nav-link" active-class="active cm-highlight" to="/echo">Echo</router-link>
+			<router-link class="navbar-item nav-link" active-class="active cm-highlight" to="/challenge">Challenge</router-link>
 		</ul>
 	</nav>
 </template>
