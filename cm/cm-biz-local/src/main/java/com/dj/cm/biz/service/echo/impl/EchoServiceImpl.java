0844693165d890d2e91a731af944c0f262e5cdc8
Sergey Lugovoy <sergey.lugovoy@resmed.com>
2019-01-17 19:42:30 +0200

    svlugovoy 16-add-echo-persistance: Extended echo service and rest

--- cm/cm-biz-local/src/main/java/com/dj/cm/biz/service/echo/impl/EchoServiceImpl.java
+++ cm/cm-biz-local/src/main/java/com/dj/cm/biz/service/echo/impl/EchoServiceImpl.java
@@ -7,8 +7,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class EchoServiceImpl implements EchoService {
 
@@ -21,12 +19,22 @@ public class EchoServiceImpl implements EchoService {
     }
 
     @Override
-    public String getEcho(Long id, int n) {
-        Optional<Echo> optionalEcho = echoRepository.findById(id);
-        if (optionalEcho.isPresent()) {
-            return doEcho(optionalEcho.get().getValue(), n);
+    public String doEcho(Long id, int n) {
+        Echo echo = getEchoById(id);
+        if (echo != null) {
+            return doEcho(echo.getValue(), n);
         } else {
             return "Not found";
         }
+    }
+
+    @Override
+    public Iterable<Echo> getAllEchos() {
+        return echoRepository.findAll();
+    }
+
+    @Override
+    public Echo getEchoById(Long id) {
+        return echoRepository.findById(id).orElse(null);
     }
 }
