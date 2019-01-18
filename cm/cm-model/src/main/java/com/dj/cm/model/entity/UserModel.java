7ec23c770c38b09a2fdeaca3e73a2c9345d7348f
Sergey Lugovoy <sergey.lugovoy@resmed.com>
2019-01-18 13:34:45 +0200

    svlugovoy 18-add-user-service Add fields to User. Update db migration script

---
+++ cm/cm-model/src/main/java/com/dj/cm/model/entity/UserModel.java
@@ -0,0 +1,106 @@
+package com.dj.cm.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+
+    private String password;
+
+    private String email;
+
+    private Boolean confirmed;
+
+    private Boolean deleted;
+
+    private Date firstLogin;
+
+    private Date lastLogin;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getConfirmed() {
+        return confirmed;
+    }
+
+    public void setConfirmed(Boolean confirmed) {
+        this.confirmed = confirmed;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public Date getFirstLogin() {
+        return firstLogin;
+    }
+
+    public void setFirstLogin(Date firstLogin) {
+        this.firstLogin = firstLogin;
+    }
+
+    public Date getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Date lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    @Override
+    public String toString() {
+        return "UserModel{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", confirmed=" + confirmed +
+                ", deleted=" + deleted +
+                ", firstLogin=" + firstLogin +
+                ", lastLogin=" + lastLogin +
+                '}';
+    }
+}
