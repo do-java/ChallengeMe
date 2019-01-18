4d34a86a563a284b0e808127f3206a2de34ce8c5
vkazhdan <vkazhdan@anotheria.net>
2019-01-18 19:13:28 +0200

    29-add-events-integration: Added alerts on echo page

---
+++ cm/cm-ui/src/main/resources/static/js/components/common/Alerts.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<div v-for="item in items" class="alert alert-primary alert-dismissible" role="alert">
+          <span>{{item}}</span>
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+		  addEventHandlerFunction: Function
+		},
+		data() {
+			return {
+				items: []
+			}
+		},
+		created() {
+			console.log('created');
+			this.addEventHandlerFunction(data => {
+				this.items.push('Event: ' + JSON.stringify(data));
+				setTimeout(() => {
+					this.items.shift();
+				}, 3000);
+			})
+		}
+	}
+</script>
--- cm/cm-ui/src/main/resources/static/js/pages/EchoPage.vue
+++ cm/cm-ui/src/main/resources/static/js/pages/EchoPage.vue
@@ -2,6 +2,7 @@
 	<div class="card">
 	   <h2 class="card-header">Echo Page</h2>
 	   <div class="card-body">
+	   	   <alerts :addEventHandlerFunction="addEventHandlerFunction"></alerts>
 		   <echo-add-form :items="echos"></echo-add-form>
 		   <hr/>
 		   <echo-list :items="echos"></echo-list>
@@ -13,17 +14,19 @@
    import EchoList from 'components/echo/EchoList.vue'
    import EchoAddForm from 'components/echo/EchoAddForm.vue'
    import EchoGet from 'components/echo/EchoGet.vue'
+   import Alerts from 'components/common/Alerts.vue'
    import { addEchoHandler } from 'util/websocket'
 
    export default {
 		components: {
 			EchoList,
 			EchoAddForm,
-			EchoGet
+			EchoGet,
+			Alerts,
 		},
 		data: function() {
 			return {
-				echos: []
+				echos: [],
 			}
 		},
 		created: function () {
@@ -32,11 +35,11 @@
 					data.forEach(item => this.echos.push(item))
 				)
 			)
-
-			addEchoHandler(data => {
-                console.log('Handler received event:');
-                console.log(data);
-            })
+		},
+		methods: {
+			addEventHandlerFunction: function(data) {
+				addEchoHandler(data);
+			}
 		}
    }
 </script>
