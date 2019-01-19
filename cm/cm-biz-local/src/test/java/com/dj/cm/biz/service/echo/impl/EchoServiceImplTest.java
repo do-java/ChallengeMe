6e65c5f7d39f470d2d9691d6b367211a5e900fe8
Sergey Lugovoy <sergey.lugovoy@resmed.com>
2019-01-19 09:37:45 +0200

    svlugovoy 26-add-inmemory-echo-service-implementation: Clear unused imports

--- cm/cm-biz-local/src/test/java/com/dj/cm/biz/service/echo/impl/EchoServiceImplTest.java
+++ cm/cm-biz-local/src/test/java/com/dj/cm/biz/service/echo/impl/EchoServiceImplTest.java
@@ -17,9 +17,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
