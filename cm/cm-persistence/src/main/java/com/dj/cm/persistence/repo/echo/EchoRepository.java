9c3bc6eb1df3aaa5943a3fde9d9bf33db77fd7c0
AndrewPopenko <andrew.popenko@gmail.com>
2019-01-19 12:08:17 +0100

    apopenko 16-add-echo-persistance

---
+++ cm/cm-persistence/src/main/java/com/dj/cm/persistence/repo/echo/EchoRepository.java
@@ -0,0 +1,9 @@
+package com.dj.cm.persistence.repo.echo;
+
+import com.dj.cm.model.entity.Echo;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EchoRepository extends CrudRepository<Echo, Long> {
+}
