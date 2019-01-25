8f1e3e9c1bd82850f70158d0f6a5a2432078a0f4
Sergey Lugovoy <sergey.lugovoy@resmed.com>
2019-01-25 20:03:10 +0200

    Text content for login page

--- cm/cm-ui/src/main/resources/static/js/pages/LoginPage.vue
+++ cm/cm-ui/src/main/resources/static/js/pages/LoginPage.vue
@@ -1,16 +1,34 @@
 <template>
     <div class="container">
 
-        <router-link to="/">Log In</router-link>
+        <router-link to="/"><span><i class="far fa-user"></i></span> Log In</router-link>
+        <hr>
 
-        <div class="card">
-            <h2 class="card-header">Login Page</h2>
-            <div class="card-body">
-                Welcome to the Login Page.
-                <hr>
-                <p>User</p>
-                <p>Password</p>
+        <div class="border text-center">
+            <router-link class="navbar-brand cm-logo" to="/">Challenge<span class="cm-logo-me">Me</span></router-link>
+            <h2>Application to create, manage, and share challenges</h2>
+            <h4>Create an account</h4>
+        </div>
+
+        <div class="row justify-content-center">
+
+            <div class="col-md-4 border">
+                <input type="text" placeholder="First and Last Name"/>
+                <input type="text" placeholder="E-mail"/>
+                <input type="text" placeholder="Password show"/>
+                <p>By registering, you are agree that you've read and accept our <a href="#">User Agreement</a></p>
+                <button>Create Account</button>
             </div>
+
+            <div class="col-md-1 border">
+                <div>or</div>
+            </div>
+
+            <div class="col-md-4 border">
+                <div class="border">Continue with Facebook</div>
+                <div class="border">Continue with Google</div>
+            </div>
+
         </div>
     </div>
 </template>
