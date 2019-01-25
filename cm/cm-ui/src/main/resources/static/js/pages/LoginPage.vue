dbb8ea05b824d6ad77a9e940c76250580677389a
Sergey Lugovoy <sergey.lugovoy@resmed.com>
2019-01-25 16:08:08 +0200

    Created Login Page without style

---
+++ cm/cm-ui/src/main/resources/static/js/pages/LoginPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+
+        <router-link to="/">Log In</router-link>
+
+        <div class="card">
+            <h2 class="card-header">Login Page</h2>
+            <div class="card-body">
+                Welcome to the Login Page.
+                <hr>
+                <p>User</p>
+                <p>Password</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
