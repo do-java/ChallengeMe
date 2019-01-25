6bfe45e643e63b5d4ba7ef03f8663f6582842ccc
vkazhdan <vkazhdan@anotheria.net>
2019-01-25 19:25:39 +0200

    vkazhdan 16-add-echo-persistance: Simplified JpaAutitingConfig, thanks to Hanna Sachok

--- cm/cm-persistence/src/main/java/com/dj/cm/persistence/config/JpaAuditingConfig.java
+++ cm/cm-persistence/src/main/java/com/dj/cm/persistence/config/JpaAuditingConfig.java
@@ -1,19 +1,10 @@
 package com.dj.cm.persistence.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.Optional;
-
 @Configuration
-@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableJpaAuditing
 public class JpaAuditingConfig {
-
-	@Bean
-	public AuditorAware<String> auditorAware() {
-		return () -> Optional.ofNullable(null);
-	}
 
 }
