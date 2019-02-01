6d298834b3776f861fdbe883ed13dafa7268f34a
Hanna Sachok <anna.sachok@gmail.com>
2019-02-01 19:53:40 +0200

    shad11 17-add-flyway-integration

--- cm/cm-model/src/main/java/com/dj/cm/model/entity/Echo.java
+++ cm/cm-model/src/main/java/com/dj/cm/model/entity/Echo.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 public class Echo {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String value;
     @CreatedDate
