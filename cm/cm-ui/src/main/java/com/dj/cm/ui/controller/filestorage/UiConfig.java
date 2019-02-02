9f8f430a628feb1fdde9a3700f0a64257029cb05
AndrewPopenko <andrew.popenko@gmail.com>
2019-02-02 09:35:22 +0100

    37-add-max-upolad-image-size

---
+++ cm/cm-ui/src/main/java/com/dj/cm/ui/controller/filestorage/UiConfig.java
@@ -0,0 +1,9 @@
+package com.dj.cm.ui.controller.filestorage;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("ui.properties")
+public class UiConfig {
+}
