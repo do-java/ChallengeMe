1b14e7826f82b567bac1402451e378393ee0e00c
Sergey Lugovoy <sergey.lugovoy@resmed.com>
2019-02-06 09:09:46 +0200

    Draft markup LoginPage completed

--- cm/cm-ui/src/main/resources/static/js/pages/LoginPage.vue
+++ cm/cm-ui/src/main/resources/static/js/pages/LoginPage.vue
@@ -1,34 +1,91 @@
 <template>
     <div class="container">
 
-        <router-link to="/"><span><i class="far fa-user"></i></span> Log In</router-link>
-        <hr>
+        <!-- Button trigger modal -->
+        <div class="row justify-content-end">
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#loginModal">
+                <span class="login-tab"><i class="far fa-user"></i> Log In</span>
+            </button>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+             aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Log In</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
 
-        <div class="border text-center">
-            <router-link class="navbar-brand cm-logo" to="/">Challenge<span class="cm-logo-me">Me</span></router-link>
-            <h2>Application to create, manage, and share challenges</h2>
-            <h4>Create an account</h4>
+                    <form>
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="email1" placeholder="E-mail">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" id="password1" placeholder="Password">
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <router-link to="/"><button type="button" class="btn btn-primary" data-dismiss="modal">Submit</button></router-link>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
         </div>
 
-        <div class="row justify-content-center">
+        <div class=" login-content">
+            <div class="row justify-content-center text-center ">
+                <router-link class="navbar-brand cm-logo" to="/">
+                    <span class="logo-in-login-area">Challenge<span
+                            class="cm-logo-me">Me</span>
+                    </span>
+                </router-link>
+                <div class="text-login-content">
+                    <h2>Application to create, manage, and share challenges</h2>
+                    <h5><a href="#">Create an account</a></h5>
+                </div>
 
-            <div class="col-md-4 border">
-                <input type="text" placeholder="First and Last Name"/>
-                <input type="text" placeholder="E-mail"/>
-                <input type="text" placeholder="Password show"/>
-                <p>By registering, you are agree that you've read and accept our <a href="#">User Agreement</a></p>
-                <button>Create Account</button>
             </div>
 
-            <div class="col-md-1 border">
-                <div>or</div>
-            </div>
+            <div class="row justify-content-center">
 
-            <div class="col-md-4 border">
-                <div class="border">Continue with Facebook</div>
-                <div class="border">Continue with Google</div>
-            </div>
+                <div class="col-md-4">
 
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="name" placeholder="First and Last Name">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="email" placeholder="E-mail">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="password" placeholder="Password">
+                        </div>
+
+                        <p>By registering, you are agree that you've read and accept our <a href="#">User Agreement</a>
+                        </p>
+
+                        <button type="submit" class="btn btn-primary mb-3 cm-highlight"><span>Create Account</span>
+                        </button>
+                    </form>
+
+                </div>
+
+                <div class="col-md-1">
+                    <div>or</div>
+                </div>
+
+                <div class="col-md-4">
+
+                    <button type="button" class="btn btn-primary mb-3">Continue with Facebook</button>
+                    <button type="button" class="btn btn-primary">Continue with Google</button>
+
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -38,5 +95,31 @@
 </script>
 
 <style>
+
+    .login-tab {
+        color: #000;
+    }
+
+    .login-content {
+        background-color: #b3cfdb;
+    }
+
+    .logo-in-login-area {
+        font-size: 1.5em;
+        color: black;
+    }
+
+    .text-login-content {
+        margin: 30px 0;
+    }
+
+    .text-login-content a {
+        color: #000;
+    }
+
+    form > p > a {
+        color: #000;
+        font-weight: bold;
+    }
 
 </style>
